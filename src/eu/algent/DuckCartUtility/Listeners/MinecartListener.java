d47862ad9c5301809db56740c6baf82c04ab5d9c
Algent <algent@gmail.com>
2012-07-28 04:05:12 +0200

    Added Feature: animals & monsters are removed on minecart collision

--- src/eu/algent/DuckCartUtility/Listeners/MinecartListener.java
+++ src/eu/algent/DuckCartUtility/Listeners/MinecartListener.java
@@ -2,31 +2,47 @@ package eu.algent.DuckCartUtility.Listeners;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Minecart;
-import org.bukkit.entity.Vehicle;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.*;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 public class MinecartListener implements Listener {
 	
 	@EventHandler
 	public void onVehicleExit(VehicleExitEvent event){
-		Vehicle vehicle = event.getVehicle();
-		if (!(vehicle instanceof Minecart)) return;
-		Minecart minecart = (Minecart) vehicle;
+		if (!(event.getVehicle() instanceof Minecart)) return;
+		Minecart minecart = (Minecart) event.getVehicle();
 		if (minecart.getDamage() > 40) return;
-		vehicle.remove();
-		Location location = vehicle.getLocation();
+		minecart.remove();
+		Location location = minecart.getLocation();
 		location.getWorld().dropItem(location, new ItemStack(Material.MINECART, 1));
 		
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
-	public void onVehicleDestroy(VehicleDestroyEvent event){
-		
+	public void onVehicleEntityCollision(VehicleEntityCollisionEvent event){
+		if (event.getVehicle() instanceof Minecart){
+			Minecart minecart = (Minecart) event.getVehicle();
+			if (event.getEntity() instanceof LivingEntity){
+				LivingEntity livingentity = (LivingEntity) event.getEntity();
+				if (!(livingentity instanceof Player) && !(livingentity instanceof Tameable)){
+					final Vector velocity = minecart.getVelocity(); 
+					if (velocity.getX() != 0d || velocity.getY() != 0d || velocity.getZ() != 0d){
+						livingentity.remove();
+						event.setCancelled(true);
+						event.setCollisionCancelled(true);
+					}
+				}
+
+			}
+		}
 	}
 	
 }
