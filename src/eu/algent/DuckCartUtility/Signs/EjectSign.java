2b260e44a62a1843beddf65321f89d999581e2c1
Algent <algent@gmail.com>
2012-08-06 05:12:40 +0200

    EjectSign: Added optional eject condition. For now only able to check id of item in passenger hand.

--- src/eu/algent/DuckCartUtility/Signs/EjectSign.java
+++ src/eu/algent/DuckCartUtility/Signs/EjectSign.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import eu.algent.DuckCartUtility.DuckCartUtility;
@@ -15,7 +16,7 @@ import eu.algent.DuckCartUtility.Utils.StringUtil;
  *  [Eject]
  *   x,y,z     (offset from cart)
  *   yaw,pitch or yaw
- *   truc ici ?
+ *   ph:item      (playerhand:item)
  */
 public class EjectSign {
     DuckCartUtility plugin;
@@ -25,8 +26,19 @@ public class EjectSign {
     }
 
     public void execute(Sign sign, Minecart minecart) {
-        if(!plugin.getPluginConfig().isEjectSignEnabled()) return;
-        
+        if (!plugin.getPluginConfig().isEjectSignEnabled()) return;
+
+        // Reading Optional Condition
+        String[] conditionLine = sign.getLine(3).split(":");
+        int idWanted = 0, idInHand = 0;
+        if (conditionLine.length == 2 && conditionLine[0].equalsIgnoreCase("ph")) {
+            idWanted = StringUtil.parseIntSafe(conditionLine[1], 0);
+            if (!minecart.isEmpty() && minecart.getPassenger() instanceof Player) {
+                idInHand = ((Player) minecart.getPassenger()).getItemInHand().getTypeId();
+                if (idWanted != idInHand) return;
+            }
+        }
+
         // Reading Offset from sign
         String[] offsetLine = sign.getLine(1).split(",");
         Vector offset = new Vector();
@@ -40,7 +52,7 @@ public class EjectSign {
         {
             offset.normalize().multiply(maxDistance);
         }
-        
+
         // Reading Orientation from sign
         String[] orientationLine = sign.getLine(2).split(",");
         float yaw = 0F, pitch = 0F;
@@ -50,13 +62,13 @@ public class EjectSign {
         } else if (orientationLine.length == 1) {
             yaw = (float) StringUtil.parseIntSafe(orientationLine[0], 0);
         }
-        
+
         // Readying eject destination
         Location destination = minecart.getLocation().getBlock().getLocation();
         destination = destination.add(0.5, 0.7, 0.5).add(offset);
         destination.setYaw(yaw + 180);
         destination.setPitch(pitch);
-        
+
         // Ejecting
         minecart.teleport(destination);
         if (!minecart.isEmpty()) {
